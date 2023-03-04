1f83c92749c4cf771b4f2d6fe24f22c0cc6ef996
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-04 19:31:27 -0300

    feat: Add Mundo Topia page

---
+++ app/mundo-topia/page.js
@@ -0,0 +1,33 @@
+import CardLink from '@/components/links/CardLink'
+import React from 'react'
+
+const items = [
+  {
+    title: "Test de personalidad.",
+    description: "¿Qué personaje de Topia eres? Averigualo en este cuestionario.",
+    path: "test-personalidad"
+  }
+]
+
+const page = () => {
+  return (
+    <main>
+      <div className='text-center mb-10'>
+        <h1 className='text-3xl mb-2 font-semibold text-[#333]'>Mundo Topia</h1>
+        <p className='text-xl text-[#333]'>Explora el universo de la novela y conoce más sobre sus personajes. </p>
+      </div>
+      <ul className='max-w-2xl m-auto'>
+        {
+          items.map(item => <li key={item.path}>
+            <CardLink href={`mundo-topia/${item.path}`}>
+              <h2 className='font-semibold mb-1 text-lg'>{item.title}</h2>
+              <p>{item.description}</p>
+            </CardLink>
+          </li>)
+        }
+      </ul>
+    </main>
+  )
+}
+
+export default page
