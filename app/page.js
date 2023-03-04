606627a2f6c285bd7af6246d3b1851f7c3d80801
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-04 02:34:16 -0300

    feat: Add page from scratch

--- app/page.js
+++ app/page.js
@@ -1,91 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import ContactLink from "@/components/links/ContactLink";
+import MercadoPagoLink from "@/components/links/MercadoPagoLink";
+import ReviewList from "@/components/reviews/ReviewList";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <main className="flex-1 flex flex-col-reverse md:flex-row items-center justify-center gap-16 px-7 mb-10">
+        <div className="flex flex-col gap-1 text-[#333]">
+          <h1 className="text-3xl font-semibold mb-3">Aventúrate en "Topia" de <br />Valeria Galbarini</h1>
+          <p className="max-w-lg text-lg mb-5">Descubre una isla misteriosa donde los personajes entrañables cobran vida y una idea revolucionaria toma forma. Topia es una novela cautivadora, capaz de transportarte a un mundo lleno de vivencias únicas que invitan a la reflexión y te hacen sentir parte de la historia.</p>
+          <div className="flex gap-3">
+            <ContactLink />
+            <MercadoPagoLink />
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Image src="/images/book-cover.webp" width={300} height={300} alt="Portada de Topia" />
+      </main>
+      <section className="px-7">
+        <ReviewList />
+      </section>
+    </div>
   )
 }
