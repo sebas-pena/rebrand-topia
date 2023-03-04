74697b21f846134eebf25ee9f81894c54040fd6c
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-04 19:26:42 -0300

    feat: Add quotation marks

--- components/reviews/ReviewItem.js
+++ components/reviews/ReviewItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ReviewItem = ({ author, content }) => {
   return (
-    <article className='p-5 border border-neutral-200 shadow-sm rounded-2xl bg-white'>
-      <p className='text-md mb-3'>{content}</p>
-      <h3 className='font-semibold text-lg text-center'>{author}</h3>
+    <article className='p-5 border border-neutral-200 shadow-sm rounded-2xl bg-white text-md text-center'>
+      <span className='mb-3'>❝ {content} ❞</span>
+      <h3 className='pl-1 font-semibold text-center'>{author}</h3>
     </article>
   )
 }
