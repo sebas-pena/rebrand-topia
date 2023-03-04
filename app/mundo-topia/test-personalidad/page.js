733b3b683d235f6b59dc4cafcff2b86684e55f0a
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-04 19:31:41 -0300

    feat: Add page

---
+++ app/mundo-topia/test-personalidad/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      <div className='text-center mb-10'>
+        <h1 className='text-3xl mb-2 font-semibold text-[#333]'>Test de personalidad</h1>
+        <p className='text-xl text-[#333]'>¿Qué personaje de Topia eres? Averigualo en este cuestionario.</p>
+      </div>
+    </main>
+  )
+}
+
+export default page
