b2c1de7c680316c03223f6975cb19292ef75d0e3
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-04 19:25:49 -0300

    feat: Change title

--- components/reviews/ReviewList.js
+++ components/reviews/ReviewList.js
@@ -5,7 +5,7 @@ import ReviewItem from './ReviewItem'
 const ReviewList = () => {
   return (
     <div className='mb-10'>
-      <h2 className="font-semibold text-2xl text-center mb-8">Reviews</h2>
+      <h2 className="font-semibold text-2xl text-center mb-8">Los primeros lectores opinan:</h2>
       <ul className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
         {
           reviews.map(review => (
