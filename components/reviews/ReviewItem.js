50b7121689cdb28e29eabba3f659c103bee1e825
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-05 00:31:16 -0300

    feat: Improve responsive

--- components/reviews/ReviewItem.js
+++ components/reviews/ReviewItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ReviewItem = ({ author, content }) => {
   return (
-    <article className='p-5 border border-neutral-200 shadow-sm rounded-2xl bg-white text-md text-center'>
+    <article className='flex flex-col justify-between p-5 border h-full border-neutral-200 shadow-sm rounded-2xl bg-white text-md text-center'>
       <span className='mb-3'>❝ {content} ❞</span>
       <h3 className='pl-1 font-semibold text-center'>{author}</h3>
     </article>
