8ffe129b86e8fee197825d89277e83393c6b0a9d
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-05 00:31:33 -0300

    feat: Improve responsive

--- app/mundo-topia/page.js
+++ app/mundo-topia/page.js
@@ -11,7 +11,7 @@ const items = [
 
 const page = () => {
   return (
-    <main>
+    <main className='px-5'>
       <div className='text-center mb-10'>
         <h1 className='text-3xl mb-2 font-semibold text-[#333]'>Mundo Topia</h1>
         <p className='text-xl text-[#333]'>Explora el universo de la novela y conoce más sobre sus personajes. </p>
