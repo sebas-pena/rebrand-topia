b29d3637b7e692f7201f55c1186bcf417a1097df
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-05 00:30:53 -0300

    feat: Improve responsive

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -8,7 +8,7 @@ const navItems = [
     path: "/prologo",
   },
   {
-    label: "Pago y Entrega",
+    label: "Preguntas",
     path: "/faq",
   },
   {
@@ -19,18 +19,18 @@ const navItems = [
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-4 px-5 border-b-2 border-neutral-800 mb-10">
+    <header className="flex justify-between items-center py-4 px-3 sm:px-4 border-b-2 border-neutral-800 mb-10">
       <Link href="/" className="inline-flex gap-3">
         <Image width={30} height={30} src="/svg/island.svg" />
-        <h2 className="text-center text-3xl font-semibold text-[#333]">
+        <h2 className="text-center text-2xl sm:3xl font-semibold text-[#333]">
           Topia
         </h2>
       </Link>
       <nav>
-        <ul className="flex gap-4">
+        <ul className="flex gap-2 sm:gap-4">
           {navItems.map((item) => (
             <li key={item.path}>
-              <Link href={item.path}>{item.label}</Link>
+              <Link className="w-max block" href={item.path}>{item.label}</Link>
             </li>
           ))}
         </ul>
