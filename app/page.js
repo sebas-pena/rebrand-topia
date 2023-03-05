c89aef44ee5cf3f4ed4dd340b1dbee99f6d673ff
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-03-05 00:30:35 -0300

    feat: Improve responsive

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <main className="flex-1 flex flex-col-reverse md:flex-row items-center justify-center gap-16 px-7 mb-10">
         <div className="flex flex-col gap-1 text-[#333]">
-          <h1 className="text-3xl font-semibold mb-3">Aventúrate en "Topia" de <br />Valeria Galbarini</h1>
+          <h1 className="text-3xl sm:max-w-sm font-semibold mb-3">Aventúrate en "Topia" de Valeria Galbarini</h1>
           <p className="max-w-lg text-lg mb-5">Descubre una isla misteriosa donde los personajes entrañables cobran vida y una idea revolucionaria toma forma. Topia es una novela cautivadora, capaz de transportarte a un mundo lleno de vivencias únicas que invitan a la reflexión y te hacen sentir parte de la historia.</p>
           <div className="flex gap-3">
             <ContactLink />
